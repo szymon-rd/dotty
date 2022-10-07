scala/scala3

New lazy vals implementation

library/src/scala/runtime/LazyVals.scala
@@ -22,6 +22,7 @@ object LazyVals {
     val processors = java.lang.Runtime.getRuntime.nn.availableProcessors()
     8 * processors * processors
   }
+
   private[this] val monitors: Array[Object] =
     Array.tabulate(base)(_ => new Object)
 
@@ -36,6 +37,41 @@ object LazyVals {
   private final val debug = false
 
   /* ------------- Start of public API ------------- */
+
+  /**
+   * Used to indicate the state of a lazy val that is being
+   * evaluated and of which other threads await the result.
+   */
+  final class Waiting:
+    private var done = false
+    
+    /**
+     * Wakes up waiting threads. Called on completion of the evaluation
+     * of lazy val's right-hand side.
+     */
+    def release(): Unit = synchronized {
+      done = true
+      notifyAll()
+    }
+
+    /**
+     * Awaits the completion of the evaluation of lazy val's right-hand side.
+     */
+    def awaitRelease(): Unit = synchronized {
+      while !done do wait()
+    }
+
+  /**
+   * Used to indicate the state of a lazy val that is currently being
+   * evaluated with no other thread awaiting its result.
+   */
+  object Evaluating
+
+  /**
+   * Used to indicate the state of a lazy val that has been evaluated to
+   * `null`.
+   */
+  object NULL
 
   final val BITS_PER_LAZY_VAL = 2L
 
@@ -52,6 +88,12 @@ object LazyVals {
     val mask = ~(LAZY_VAL_MASK << ord * BITS_PER_LAZY_VAL)
     val n = (e & mask) | (v.toLong << (ord * BITS_PER_LAZY_VAL))
     unsafe.compareAndSwapLong(t, offset, e, n)
+  }
+
+  def objCAS(t: Object, offset: Long, exp: Object, n: Object): Boolean = {
+    if (debug)
+      println(s"objCAS($t, $exp, $n)")
+    unsafe.compareAndSwapObject(t, offset, exp, n)
   }
 
   def setFlag(t: Object, offset: Long, v: Int, ord: Int): Unit = {
@@ -106,6 +148,13 @@ object LazyVals {
     r
   }
 
+  def getStaticOffset(clz: Class[_], name: String): Long = {
+    val r = unsafe.staticFieldOffset(clz.getDeclaredField(name))
+    if (debug)
+      println(s"getStaticOffset($clz, $name) = $r")
+    r
+  }
+
   def getOffsetStatic(field: java.lang.reflect.Field) =
     val r = unsafe.objectFieldOffset(field)
     if (debug)
@@ -114,11 +163,18 @@ object LazyVals {
 
 
   object Names {
+    final val waiting = "Waiting"
+    final val evaluating = "Evaluating"
+    final val nullValued = "NULL"
+    final val waitingAwaitRelease = "awaitRelease"
+    final val waitingRelease = "release"
     final val state = "STATE"
     final val cas = "CAS"
+    final val objCas = "objCAS"
     final val setFlag = "setFlag"
     final val wait4Notification = "wait4Notification"
     final val get = "get"
     final val getOffset = "getOffset"
+    final val getStaticOffset = "getStaticOffset"
   }
 }

szymon-rd/dotty

library/src/scala/runtime/LazyVals.scala
@@ -25,6 +25,7 @@ object LazyVals {
     val processors = java.lang.Runtime.getRuntime.nn.availableProcessors()
     8 * processors * processors
   }
+
   private[this] val monitors: Array[Object] =
     Array.tabulate(base)(_ => new Object)
 
@@ -39,6 +40,41 @@ object LazyVals {
   private final val debug = false
 
   /* ------------- Start of public API ------------- */
+
+  /**
+   * Used to indicate the state of a lazy val that is being
+   * evaluated and of which other threads await the result.
+   */
+  final class Waiting:
+    private var done = false
+    
+    /**
+     * Wakes up waiting threads. Called on completion of the evaluation
+     * of lazy val's right-hand side.
+     */
+    def release(): Unit = synchronized {
+      done = true
+      notifyAll()
+    }
+
+    /**
+     * Awaits the completion of the evaluation of lazy val's right-hand side.
+     */
+    def awaitRelease(): Unit = synchronized {
+      while !done do wait()
+    }
+
+  /**
+   * Used to indicate the state of a lazy val that is currently being
+   * evaluated with no other thread awaiting its result.
+   */
+  object Evaluating
+
+  /**
+   * Used to indicate the state of a lazy val that has been evaluated to
+   * `null`.
+   */
+  object NULL
 
   final val BITS_PER_LAZY_VAL = 2L
 
@@ -55,6 +91,12 @@ object LazyVals {
     val mask = ~(LAZY_VAL_MASK << ord * BITS_PER_LAZY_VAL)
     val n = (e & mask) | (v.toLong << (ord * BITS_PER_LAZY_VAL))
     unsafe.compareAndSwapLong(t, offset, e, n)
+  }
+
+  def objCAS(t: Object, offset: Long, exp: Object, n: Object): Boolean = {
+    if (debug)
+      println(s"objCAS($t, $exp, $n)")
+    unsafe.compareAndSwapObject(t, offset, exp, n)
   }
 
   def setFlag(t: Object, offset: Long, v: Int, ord: Int): Unit = {
@@ -110,6 +152,13 @@ object LazyVals {
     r
   }
 
+  def getStaticOffset(clz: Class[_], name: String): Long = {
+    val r = unsafe.staticFieldOffset(clz.getDeclaredField(name))
+    if (debug)
+      println(s"getStaticOffset($clz, $name) = $r")
+    r
+  }
+
   def getOffsetStatic(field: java.lang.reflect.Field) =
     @nowarn
     val r = unsafe.objectFieldOffset(field)
@@ -119,11 +168,18 @@ object LazyVals {
 
 
   object Names {
+    final val waiting = "Waiting"
+    final val evaluating = "Evaluating"
+    final val nullValue = "NULL"
+    final val waitingAwaitRelease = "awaitRelease"
+    final val waitingRelease = "release"
     final val state = "STATE"
     final val cas = "CAS"
+    final val objCas = "objCAS"
     final val setFlag = "setFlag"
     final val wait4Notification = "wait4Notification"
     final val get = "get"
     final val getOffset = "getOffset"
+    final val getStaticOffset = "getStaticOffset"
   }
 }